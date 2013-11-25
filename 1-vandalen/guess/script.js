12e4bff4c81048db828662333f3c5a34b7a3950e
OpenShift guest <as223jx@student.lnu.se>
2013-11-25 08:51:12 -0500

    Fixat klart Gissa Hemliga Talet

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,37 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor( Math.random() * (100-1)+1) + 1;
+	var count = 0;
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+		
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+         if(number < 1 || number > 100)
+            {
+                return [false, "Talet är utanför intervallet 0 - 100"];
+            }
+            
+            if(number < secret)
+            {
+                count += 1;
+                return [false, "Det hemliga talet är högre!"];
+            }
+            
+            if(number > secret)
+            {
+                count += 1;
+                return [false, "Det hemliga talet är lägre!"];
+            }
+            
+            if(number == secret)
+            {
+                count += 1;
+                return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + count + " gissningar för att hitta det."]
+            }
 	};
 	
 	// ------------------------------------------------------------------------------
