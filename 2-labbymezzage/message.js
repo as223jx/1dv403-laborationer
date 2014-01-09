941da72849f53fdd7a34aa22395447184ffa0573
OpenShift guest <as223jx@student.lnu.se>
2014-01-09 08:37:38 -0500

    Ändrade lite i hur tabellen byggs upp

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -28,7 +28,63 @@ Message.prototype.getHTMLtext = function() {
 };
 
 Message.prototype.getDateText = function() {
-    return this.getDate();
+    
+    var month = this.getDate().getMonth();
+    var year = this.getDate().getFullYear();
+    var date = this.getDate().getDate();
+    var hour = this.getDate().getHours();
+    var minutes = this.getDate().getMinutes();
+    var seconds = this.getDate().getSeconds();
+    
+    if(month === 0){
+        month = "Januari";
+    }
+    
+    if(month === 1){
+        month = "Februari";
+    }
+    
+    if(month === 2){
+        month = "Mars";
+    }
+    
+    if(month === 3){
+        month = "April";
+    }
+    
+    if(month === 4){
+        month = "Maj";
+    }
+    
+    if(month === 5){
+        month = "Juni";
+    }
+    
+    if(month === 6){
+        month = "Juli";
+    }
+    
+    if(month === 7){
+        month = "Augusti";
+    }
+    
+    if(month === 8){
+        month = "September";
+    }
+    
+    if(month === 9){
+        month = "Oktober";
+    }
+    
+    if(month === 10){
+        month = "November";
+    }
+    
+    if(month === 11){
+        month = "December";
+    }
+    
+    return "Inlägget skapades den: " + date + " " + month + " " + year + " klockan " + hour + ":" + minutes + ":" + seconds;
 };
 
 Message.prototype.getTimeText = function() {
--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,41 +3,75 @@
 var Memory = {
     random: [],
     
+    arr: [],
+    
+    cells: [],
+    
     rows: 4,
     cols: 4,
     
     init:function(e){
         var newRandom = new RandomGenerator.getPictureArray(Memory.rows, Memory.cols);
+        var i;
         Memory.random.push(newRandom);
         
-        
+        for (i = 0; i < newRandom.length; i++){
+            // var picture = document.createElement("img");
+            // picture.setAttribute("src", "pics/"+newRandom[i]+".png");
+            var picture = "pics/"+newRandom[i]+".png";
+            Memory.arr.push(picture);
+        }
         console.log(Memory.random);
-        console.log(Memory.random.length);
+        console.log(Memory.arr[1]);
+        console.log(Memory.arr[2]);
         
         Memory.createTable(Memory.rows, Memory.cols);
     },
     
     createTable:function(rows, cols){
+        var counter = 0;
         var table = document.createElement("table");
         var tbody = document.createElement("tbody");
         table.appendChild(tbody);
         
         for(var i = 0; i < rows; i+=1){
             var tr = document.createElement("tr");
+            
             for(var j = 0; j < cols; j+=1){
-                var td = document.createElement("td");
-                var pic = document.createElement("img");
-                pic.setAttribute("src", "pics/0.png");
-                td.appendChild(pic);
-                tr.appendChild(td);
+                
+                
+                tr.appendChild(Memory.newTd(counter));
+                // var picTwo = document.createElement("img");
+                // picTwo.setAttribute("src", Memory.arr[counter]);
+                
+                console.log("Counter: " + counter);
+                
+                counter +=1;
             }
+            
             tbody.appendChild(tr);
         }
         var memoryDiv = document.getElementById("memoryDiv");
         
-        
         memoryDiv.appendChild(table);
-        console.log(table);
+        
+    },
+    
+    newTd:function(counter){
+        var varTd = document.createElement("td");
+        varTd.setAttribute("id", counter);
+        var pic = document.createElement("img");
+        pic.setAttribute("src", "pics/0.png");
+        var a = document.createElement("a");
+        a.setAttribute("href", "#");
+        varTd.appendChild(a);
+        a.appendChild(pic);
+        
+        a.onclick = function(){
+            pic.setAttribute("src", "pics/" + counter + ".png");
+        };
+        
+        return varTd;
     }
 };
 
