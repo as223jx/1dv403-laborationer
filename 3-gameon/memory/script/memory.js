0665ce123626163e8679dda78da2a911e40cb8bb
OpenShift guest <as223jx@student.lnu.se>
2014-01-13 10:27:07 -0500

    Fixat så bilderna vänder tillbaks sig

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,7 +5,7 @@ var Memory = {
     
     arr: [],
     
-    cells: [],
+    open: [],
     
     rows: 4,
     cols: 4,
@@ -15,15 +15,11 @@ var Memory = {
         var i;
         Memory.random.push(newRandom);
         
+        
         for (i = 0; i < newRandom.length; i++){
-            // var picture = document.createElement("img");
-            // picture.setAttribute("src", "pics/"+newRandom[i]+".png");
             var picture = "pics/"+newRandom[i]+".png";
             Memory.arr.push(picture);
         }
-        console.log(Memory.random);
-        console.log(Memory.arr[1]);
-        console.log(Memory.arr[2]);
         
         Memory.createTable(Memory.rows, Memory.cols);
     },
@@ -34,45 +30,69 @@ var Memory = {
         var tbody = document.createElement("tbody");
         table.appendChild(tbody);
         
+        var memoryDiv = document.getElementById("memoryDiv");
+        
+        memoryDiv.appendChild(table);
+        
         for(var i = 0; i < rows; i+=1){
             var tr = document.createElement("tr");
             
             for(var j = 0; j < cols; j+=1){
                 
                 
-                tr.appendChild(Memory.newTd(counter));
+                tr.appendChild(Memory.newTd(counter, memoryDiv));
                 // var picTwo = document.createElement("img");
                 // picTwo.setAttribute("src", Memory.arr[counter]);
-                
-                console.log("Counter: " + counter);
                 
                 counter +=1;
             }
             
             tbody.appendChild(tr);
         }
-        var memoryDiv = document.getElementById("memoryDiv");
         
-        memoryDiv.appendChild(table);
         
     },
     
-    newTd:function(counter){
+    newTd:function(counter, memoryDiv){
         var varTd = document.createElement("td");
-        varTd.setAttribute("id", counter);
         var pic = document.createElement("img");
         pic.setAttribute("src", "pics/0.png");
+        pic.setAttribute("id", counter);
         var a = document.createElement("a");
         a.setAttribute("href", "#");
         varTd.appendChild(a);
         a.appendChild(pic);
         
         a.onclick = function(){
-            pic.setAttribute("src", "pics/" + counter + ".png");
+            
+            pic.setAttribute("src", Memory.arr[counter]);
+            
+            Memory.open.push(counter);
+            
+            console.log("Öppen 1: " + Memory.open[0]);
+            console.log("Öppen 2: " + Memory.open[1]);
+            
+            if(Memory.open.length === 2){
+                if(Memory.arr[Memory.open[0]] === Memory.arr[Memory.open[1]]){
+                    console.log("Grattis!");
+                    Memory.open = [];
+                }
+                else{
+                    console.log("Feeeeel");
+                    setTimeout(function() {
+                        document.getElementById(Memory.open[0]).setAttribute("src", "pics/0.png");
+                        document.getElementById(Memory.open[1]).setAttribute("src", "pics/0.png");                        
+                    }, 500);
+
+                    Memory.open = [];
+                }
+            }
         };
         
         return varTd;
-    }
+    },
+    
+    
 };
 
 window.onload = Memory.init;
