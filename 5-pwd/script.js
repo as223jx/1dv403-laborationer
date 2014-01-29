7ecad020e316d4c31ae3d040f291ae7fe434ec9a
OpenShift guest <as223jx@student.lnu.se>
2014-01-29 15:13:08 -0500

    Fixat lite med utseendet på popuprutan

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -6,9 +6,15 @@ var Desktop = {
         var pic = document.getElementById("pic");
         
         pic.onclick = function(){
+            
+            //Skapar popuprutan
             var container = document.getElementById("container");
             var div = document.createElement("div");
+            var head = document.createElement("div");
+            var thumb = document.createElement("img");
+            head.setAttribute("id", "head");
             div.setAttribute("id", "popup");
+            div.appendChild(head);
             
             container.appendChild(div);
         };
