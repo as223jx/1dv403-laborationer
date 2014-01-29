f3e22ea2b6e5d6e6270627895583653f839b7b45
OpenShift guest <as223jx@student.lnu.se>
2014-01-29 14:46:31 -0500

    Lagt till en knapp som öppnar ett fönster

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+var Desktop = {
+    
+    init:function(){
+        var pic = document.getElementById("pic");
+        
+        pic.onclick = function(){
+            var container = document.getElementById("container");
+            var div = document.createElement("div");
+            div.setAttribute("id", "popup");
+            
+            container.appendChild(div);
+        };
+    },
+};
+
+window.onload = Desktop.init;
